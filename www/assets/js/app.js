a65a403408531e582edeff0f36bfe8de06b04b60
Mustafa Vohra <mustafa@desiredsoft.net>
2018-08-02 16:00:11 +0530

    url

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -14,6 +14,46 @@ var app = {
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
         bizzlerApp.deviceReady = true;
+        $timeout(function(){handleOpenURL();},0)
         angular.bootstrap(document, ['bizzlerApp']);
     }
+};
+function handleOpenURL(url) {
+  // do stuff, for example
+  // document.getElementById("url").value = url;
+  console.log('checkingmail',url);
+  /*if(typeof(url) !== 'undefined' && url.indexOf('check-mail') !== -1){
+    var getToken = url.split('/check-mail/')[1];
+    getToken = getToken.replace('token=','');
+    notiMsg(getToken);
+    console.log(url);
+    showLoader(true);
+    notiMsg('Verifying your email ID');
+    var params = '?action=check-token&token='+getToken;
+    jQuery.ajax({type:'POST',url:dbURL+params}).done(function(r){
+      console.log(r);
+        notiMsg(r.message);
+        if(r.code == 404){
+          jQuery.get('screen-01.html',function(data){
+          jQuery('.content-changer').html(data);
+            hideLoader(true);
+          });
+        }
+        else{
+          currentUserId = r.user_id;
+          lcl.currentUserId = currentUserId;
+          jQuery.get('screen-03-01.html',function(data){
+            jQuery('.content-changer').html(data);
+            hideLoader(true);
+          });
+        }
+    });
+  }
+  else{
+    console.log(url);
+    console.log('checkingmail');
+    jQuery.get('screen-01.html',function(data){
+      jQuery('.content-changer').html(data);
+    });
+  }*/
 };
